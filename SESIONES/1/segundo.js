2900f789a822a64201e54df58ba9db915a314d80
JuanSalgadoUninorte <jdsa1822@gmail.com>
2021-11-09 20:35:52 -0500

    Segundo commit, operaciones matemáticas básicas

--- SESIONES/1/segundo.js
+++ SESIONES/1/segundo.js
@@ -1,4 +1,19 @@
 function sumar(a, b) {
     return a + b;
 }
-console.log(sumar(1, 2));
+function restar(a, b) {
+    return a - b;
+}
+function mul(a, b) {
+    return a * b;
+}
+function div(a, b) {
+    if(b == 0){
+        return "Error, no se puede dividir por 0";
+    }else
+    return a / b;
+}
+exports.sumar = sumar;
+exports.restar = restar;
+exports.mul = mul;
+exports.div = div;
---
+++ SESIONES/1/tercero.js
@@ -0,0 +1,5 @@
+const variable = require("./segundo");
+console.log(variable.sumar(1, 2));
+console.log(variable.restar(1, 2));
+console.log(variable.mul(1, 2));
+console.log(variable.div(1, 2));
