ff7bc0baeb0a078a6cd0e1daf6eb516041998a1b
JuanSalgadoUninorte <jdsalgado77@misena.edu.co>
2021-12-16 17:48:53 -0500

    parte15

--- SESIONES/11/Back-end/modelo/usuarioModelo.js
+++ SESIONES/11/Back-end/modelo/usuarioModelo.js
@@ -1,15 +1,23 @@
+const { genSalt, hash } = require('bcrypt');
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 const usuarioSchema = new Schema(
     {
-        Usuario: { type: "string", unique: true, required: true, max: 100},
+        usuario: { type: "string", unique: true, required: true, max: 100},
 
-        Password: { type: "string", required: true},
+        password: { type: "string", required: true},
         
-        Rol: { type: "string", required: true}
+        rol: { type: "string", required: true}
     }
 );
+usuarioSchema.pre("save", async function(next){
+    const salt = await genSalt(10);
+    console.log(salt);
+    this.password = await hash(this.password, salt)
+    console.log(this.password);
+    next()
+});
 //Modelo para base de datos => Guarda un esquema "formato" para poder ingresar la colección
 // module.exports = mongoose.model("Jugadores", usuarioModelo);
 //Crear un esquema
